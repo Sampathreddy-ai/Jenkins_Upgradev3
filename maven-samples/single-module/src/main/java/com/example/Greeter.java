caeabb5038de538561f2e9e25634705cec60d248
Sampathreddy-ai <beeramsampathkumarreddy5@gmail.com>
2025-02-14 22:27:46 +0530

    Update Greeter.java

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,7 +21,7 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello from Main Method & Hello I am doing pratice for this project !!!");
+	  System.out.println("Hello from Main Method & Hello I am doing well hi hello !!!");
   }
 	
 }
